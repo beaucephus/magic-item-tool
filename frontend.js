4f660864ee03280c87e6af2a6afb0ee0250636df
Joseph Browning <joseph.browning@macys.com>
2019-07-22 14:36:40 -0400

    Add submit checking for Magic Item Form.

--- frontend.js
+++ frontend.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/magic-item-tool'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/magic-item-tool/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/magic-item-tool/index.html'));
 });
 
 // Start our frontend
