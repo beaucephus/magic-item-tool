ebdbd6229c3532fb0e87a2971463751cc48dd64e
Joseph Browning <joseph.browning@macys.com>
2019-07-25 16:46:12 -0400

    Add forcedomain to express.

--- frontend.js
+++ frontend.js
@@ -1,10 +1,17 @@
 //Install express server
 const express = require('express');
+const forceDomain = require('forcedomain');
 const path = require('path');
 const host = '0.0.0.0';
 const port = process.env.PORT || '4200';
 
 const app = express();
+
+// Redirect non-www domain requests to www. domains.
+app.use(forceDomain({
+  hostname: 'www.magicitemtool.com',
+  excludeRule: /[a-zA-Z0-9][a-zA-Z0-9-]+\.herokuapp\.com/i
+}));
 
 // Redirect all frontend traffic to HTTPS except requests for localhost.
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
