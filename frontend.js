a38f9170b40c0594d2336d7fd3fca8062d854858
Joseph Browning <joseph.browning@macys.com>
2019-07-25 16:53:37 -0400

    Use forceDomain for HTTPS redirects as well.

--- frontend.js
+++ frontend.js
@@ -4,18 +4,14 @@ const forceDomain = require('forcedomain');
 const path = require('path');
 const host = '0.0.0.0';
 const port = process.env.PORT || '4200';
-
 const app = express();
 
 // Redirect non-www domain requests to www. domains.
 app.use(forceDomain({
   hostname: 'www.magicitemtool.com',
+  protocol: 'https',
   excludeRule: /[a-zA-Z0-9][a-zA-Z0-9-]+\.herokuapp\.com/i
 }));
-
-// Redirect all frontend traffic to HTTPS except requests for localhost.
-const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
-app.use(redirectToHTTPS([/localhost:4200/]));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/magic-item-tool'));
